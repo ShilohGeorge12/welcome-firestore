6f84259656084ea6e4b108d52c0b00c323244bd6
Shiloh George <shinohgeorge2019@gmail.com>
2024-10-18 15:15:09 +0100

    - Learnt query delete by name - Learnt sorting when data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { addDoc, collection, deleteDoc, doc, DocumentData, onSnapshot, setDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, DocumentData, onSnapshot, orderBy, query, serverTimestamp, updateDoc } from 'firebase/firestore';
 import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 
 import { Dot } from './components/dot';
@@ -12,42 +12,50 @@ function App() {
 		name: '',
 		value: '',
 	};
+	const [showAddColorFormState, setShowAddColorFormState] = useState<boolean>(true);
 	const dialogRef = useRef<HTMLDialogElement | null>(null);
 	const [colors, setColors] = useState<colorsType>([]);
-	const [formState, setFormState] = useState<typeof initFormState>(initFormState);
+	const [addColorFormState, setAddColorFormState] = useState<typeof initFormState>(initFormState);
 	const [editFormState, setEditFormState] = useState<{ name: string; value: string; id: string }>({ id: '', name: '', value: '' });
+	const [deleteQueryState, setDeleteQueryState] = useState<{ name: string }>({ name: '' });
 
-	useEffect(
-		() =>
-			onSnapshot(collection(db, 'colors'), (snapshot) => {
-				let v: colorsType = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-				setColors(v);
-			}),
-		[]
-	);
+	useEffect(() => {
+		const sortingQuery = query(collection(db, 'colors'), orderBy('timeStamp', 'desc'));
+		const unSub = onSnapshot(sortingQuery, (snapshot) => {
+			let v: colorsType = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+			setColors(v);
+		});
+		return unSub;
+	}, []);
 
 	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
-		setFormState((prev) => ({ ...prev, [name]: value }));
+		setAddColorFormState((prev) => ({ ...prev, [name]: value }));
 	};
+
 	const onInputEditChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
 		setEditFormState((prev) => ({ ...prev, [name]: value }));
+	};
+
+	const onDeleteInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setDeleteQueryState((prev) => ({ ...prev, name: value }));
 	};
 
 	const onEditColorSubmit = async (e: FormEvent<HTMLFormElement>, id: string) => {
 		e.preventDefault();
 		const docRef = doc(db, 'colors', id);
 		const payload = { name: editFormState.name, value: editFormState.value };
-		await setDoc(docRef, payload);
+		await updateDoc(docRef, payload);
 	};
 
-	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+	const onSubmitNewColor = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const collectionRef = collection(db, 'colors');
-		const payload = { name: formState.name, value: formState.value };
+		const payload = { name: addColorFormState.name, value: addColorFormState.value, timeStamp: serverTimestamp() };
 		await addDoc(collectionRef, payload);
-		setFormState(initFormState);
+		setAddColorFormState(initFormState);
 	};
 
 	const onDeleteColor = async (id: string) => {
@@ -55,26 +63,57 @@ function App() {
 		await deleteDoc(docRef);
 	};
 
+	const onQueryDelete = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		colors.forEach(async (color) => {
+			if (color.name === deleteQueryState.name) {
+				const docRef = doc(db, 'colors', color.id);
+				await deleteDoc(docRef);
+			}
+		});
+
+		setDeleteQueryState({ name: '' });
+	};
+
 	return (
 		<section className="root">
-			<form onSubmit={onSubmit}>
-				<input
-					type="text"
-					name="name"
-					required
-					value={formState.name}
-					onChange={onInputChange}
-				/>
-				<input
-					type="text"
-					name="value"
-					required
-					value={formState.value}
-					onChange={onInputChange}
-				/>
+			<button
+				type="button"
+				onClick={() => setShowAddColorFormState((prev) => !prev)}>
+				{showAddColorFormState ? 'Show Color Delete Query' : 'Show Add Color Form'}
+			</button>
+			{showAddColorFormState && (
+				<form onSubmit={onSubmitNewColor}>
+					<input
+						type="text"
+						name="name"
+						required
+						value={addColorFormState.name}
+						onChange={onInputChange}
+					/>
+					<input
+						type="text"
+						name="value"
+						required
+						value={addColorFormState.value}
+						onChange={onInputChange}
+					/>
 
-				<button type="submit">New</button>
-			</form>
+					<button type="submit">New</button>
+				</form>
+			)}
+
+			{!showAddColorFormState && (
+				<form onSubmit={onQueryDelete}>
+					<input
+						required
+						type="text"
+						value={deleteQueryState.name}
+						onChange={onDeleteInputChange}
+					/>
+					<button type="submit">Delete All Colors By Name</button>
+				</form>
+			)}
 
 			<ul>
 				{colors.length > 0 &&
